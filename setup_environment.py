d67a2b2df0069232abf9fbea4cc6f34ccdd3fc2d
유저이름 <ahwlsqja1324@gmail.com>
2024-11-16 04:38:19 +0900

    feat/visualization2

--- setup_environment.py
+++ setup_environment.py
@@ -1,17 +1,16 @@
 import os
 from datetime import date
-
-from decouple import config
-
+from dotenv import load_dotenv  # python-dotenv import
 
 def set_environment_variables(project_name: str = "") -> None:
     if not project_name:
         project_name = f"Test_{date.today()}"
 
-    os.environ["OPENAI_API_KEY"] = str(config("OPENAI_API_KEY"))
+    load_dotenv()  # .env 파일 로드
 
+    os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
     os.environ["LANGCHAIN_TRACING_V2"] = "true"
-    os.environ["LANGCHAIN_API_KEY"] = str(config("LANGCHAIN_API_KEY"))
+    os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
     os.environ["LANGCHAIN_PROJECT"] = project_name
 
     print("API Keys loaded and tracing set with project name: ", project_name)
